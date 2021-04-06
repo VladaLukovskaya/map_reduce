fac4569405860fcaff240435445f493dea57d4ef
VladaLukovskaya <vladusik.l@yandex.ru>
2021-04-06 09:11:17 -0900

    reducers were edited

--- reducer_pairs.py
+++ reducer_pairs.py
@@ -1,13 +1,26 @@
 import sys
 
-pairs_with_count = dict()
-for line in sys.stdin:
-    line = line.strip()
-    pair, count = line.split('\t')
-    if not pairs_with_count.get(pair):
-        pairs_with_count[pair] = int(count)
-    else:
-        pairs_with_count[pair] += int(count)
+# pairs_with_count = dict()
+# for line in sys.stdin:
+#     line = line.strip()
+#     pair, count = line.split('\t')
+#     if not pairs_with_count.get(pair):
+#         pairs_with_count[pair] = int(count)
+#     else:
+#         pairs_with_count[pair] += int(count)
 
-for pair, count in pairs_with_count.items():
-    print(pair, count, sep='\t')
+(pair, count) = (None, 0)
+
+for line in sys.stdin:
+    (key, value) = line.split('\t')
+    if pair and pair != key:
+        print(f'{pair}\t{count}')
+        (pair, count) = (key, int(value))
+    else:
+        (pair, count) = (key, count + int(value))
+
+if pair:
+    print(f'{pair}\t{str(count)}')
+
+# for pair, count in :
+#     print(pair, count, sep='\t')
--- reducer_stripes.py
+++ reducer_stripes.py
@@ -1,39 +1,49 @@
 import sys
-import json
+# import json
 
-items_and_stripes = dict()
+# items_and_stripes = dict()
+
+(item, stripe) = (None, dict())
 
 
-def dict_sum(new_item, new_stripe: dict):
+def dict_sum(new_stripe):
+
+    # stripe_sum = sum(new_stripe.values())
+    # for internal_key, internal_value in new_stripe.items():
+    #     stripe_sum += internal_value
+    #     print(stripe_sum)
+    #     return {internal_key: internal_key}
+    #
+    items_and_stripes = dict()
     new_keys = new_stripe.keys()
-    if new_item in items_and_stripes.keys():
-        stripe = items_and_stripes.get(new_item)
-        if stripe:
-            for key in new_keys:
-                if key in stripe.keys():
-                    stripe[key] += new_stripe[key]
-                else:
-                    stripe[key] = new_stripe[key]
-    else:
-        items_and_stripes[new_item] = new_stripe
+    print('new_stripe', new_keys)
+    for elem in new_keys:
+        if elem in stripe.keys():
+            stripe[elem] += new_stripe[elem]
+        else:
+            stripe[elem] = new_stripe[elem]
+    print()
 
 
 for line in sys.stdin:
     line = line.strip()
-    item, stripe = line.split('\t')
-    if stripe != '{}':
-        dict_sum(item, json.loads(stripe))
-    # print(item, stripe)
-    # if stripe != "{}":
-    #     if items_and_stripes.get(item):
-    #         new_stripe = items_and_stripes[item] + ' ' + stripe
-    #         new_stripe.split(' ')
-    #         new_pair = {item: new_stripe}
-    #         items_and_stripes.update(new_pair)
-    #     else:
-    #         items_and_stripes[item] = stripe
+    (key, value) = line.strip().split('\t')
+    value_dict = eval(value)
+    print('new', key, value)
+    if value != '{}':
+        if item and item != key:
+            print(f'{item}\t{str(dict_sum(stripe))}')
+            stripe = value_dict
+        else:
+            for k in stripe.keys():
+                stripe[k] += value_dict[k]
+        item = key
+    #
+    #     dict_sum(item, json.loads(stripe))
 
-# pp = pprint.PrettyPrinter()
-# pp.pprint(items_and_stripes)
-for key, value in items_and_stripes.items():
-    print(key, json.dumps(value), sep='\t')
+if item:
+    print(f'{item}\t{str(dict_sum(stripe))}')
+
+
+# for key, value in items_and_stripes.items():
+#     print(key, json.dumps(value), sep='\t')
